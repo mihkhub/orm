6f2923b1ca1d4fafe36233d6d623c88a33eda8a9
Issac <changfeng.or.chen79@gmail.com>
2017-06-29 22:48:34 +0800

    SoA data  structure

---
+++ ch04/4.5/4.5.1/soa/soa.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+#define NumOfVertices 1024
+
+typedef struct
+{
+	float x[NumOfVertices];
+	float y[NumOfVertices];
+	float z[NumOfVertices];
+	int a[NumOfVertices];
+	int b[NumOfVertices];
+	int c[NumOfVertices];
+} VerticesList;
+
+VerticesList Vertices;
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
